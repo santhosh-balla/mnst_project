9a87da5df5bc601beadc631d18d89c6e8851710e
santhosh-balla <satyasanthosh31@gmail.com>
2026-02-24 12:11:53 -0500

    layout changes, added canvas

--- mnst_project/app/page.tsx
+++ mnst_project/app/page.tsx
@@ -1,23 +1,40 @@
+"use client";
 import "./globals.css";
 
+import { useState } from "react";
+
+import { useRef } from "react";
+
 export default function Home() {
+
+
+  const canvasRef = useRef(null);
+  
+
   return (
-    <div className = 'container'>
-    <div className = 'container-name'>
-      <div style = {{fontWeight: 'bold'}}>DRAW HERE</div>
-      <div style = {{height: '60vh', border: '5px solid black'}} > </div>
-      <div style = {{padding: '10px'}}className = 'container'>
-      <button> Reset </button>
-      <button> Upload </button>
+    <div className="container">
+      <div className="container-name">
+        <div style={{ fontWeight: "bold" }}>DRAW HERE</div>
+        <div style={{ height: "60vh", border: "5px solid black" }}>
+          <canvas
+            ref={canvasRef}
+            className="pointer"
+            style={{
+              height: "100%",
+              width: "100%",
+              border: "solid #aa9898",
+            }}
+          ></canvas>
+        </div>
+        <div style={{ padding: "10px" }} className="container">
+          <button> Reset </button>
+          <button> Upload </button>
+        </div>
       </div>
-    </div>
-    <div className = 'container-name'>
-      <div style = {{fontWeight: 'bold'}}>THE NUMBER THE MODEL GUESSED:</div>
-      <div style = {{height: '60vh', border: '5px solid black'}}></div>
-    </div>
+      <div className="container-name">
+        <div style={{ fontWeight: "bold" }}>THE NUMBER THE MODEL GUESSED:</div>
+        <div style={{ height: "60vh", border: "5px solid black" }}></div>
+      </div>
     </div>
   );
 }
-
-
-
